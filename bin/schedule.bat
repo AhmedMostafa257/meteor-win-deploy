af9bfe00f3e307c30c19fcef27e0d822e0172e7b
Ahmed Mostafa Gomaa <ahmedmostafa257@gmail.com>
2018-07-23 09:08:23 +0200

    Tiny fix in create scheduled tasks

--- bin/schedule.bat
+++ bin/schedule.bat
@@ -1,4 +1,4 @@
-schTasks /create /tn "MongoDB database continous backup" /tr "wscript.exe %SystemDrive%\script\hide_backup.vbs" /sc HOURLY /mo 2
-schTasks /create /tn "Meteor helper application" /tr "wscript.exe %SystemDrive%\script\hide_helper.vbs" /sc ONLOGON
+schTasks /create /tn "MongoDB database continous backup" /tr "wscript.exe %SystemDrive%\scripts\hide_backup.vbs" /sc HOURLY /mo 2
+schTasks /create /tn "Meteor helper application" /tr "wscript.exe %SystemDrive%\scripts\hide_helper.vbs" /sc ONLOGON
 
 pause

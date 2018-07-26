fa35c8827dd9efc7f2e3354990664bacaa06605b
Ahmed Mostafa Gomaa <ahmedmostafa257@gmail.com>
2018-07-26 12:19:04 +0200

    Fix bug related to overwrite log file when reopened in same minute

--- bin/run.bat
+++ bin/run.bat
@@ -2,9 +2,11 @@
 
 for /f "tokens=2-4 delims=/ " %%a in ('date /t') do (set mydate=%%c-%%b-%%a)
 for /f "tokens=1-2 delims=/:" %%a in ("%TIME%") do (set mytime=%%a%%b)
-set LOGFILENAME=%mydate%_%mytime%.log
+set LOGFILETIME=%mydate%_%mytime%
 
 for %%f in (%1) do set FILENAME=%%~nf
+
+set /A COUNT=0
 
 if exist "%SystemDrive%\log" goto RUNPATCH
 echo Log directory not found
@@ -13,4 +15,14 @@ mkdir "%SystemDrive%\log"
 echo.
 
 :RUNPATCH
-cmd /c %1 > %SystemDrive%\log\%FILENAME%_%LOGFILENAME% 2>&1
+
+set /A COUNT+=1
+set "LOGFILEPATH=%SystemDrive%\log\%FILENAME%_%LOGFILETIME%-%COUNT%.log"
+
+if exist "%LOGFILEPATH%" (
+  echo Log files exists
+  echo.
+  goto RUNPATCH
+)
+
+cmd /c %1 > "%LOGFILEPATH%" 2>&1

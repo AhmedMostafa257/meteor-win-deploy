e1d2c183fce81db6a6ddf5fd97e7f0333999110e
Ahmed Mostafa Gomaa <ahmedmostafa257@gmail.com>
2018-07-30 14:05:45 +0200

    Fix overwriting scheduled tasks

--- bin/schedule.bat
+++ bin/schedule.bat
@@ -1,6 +1,17 @@
 @echo off
 
+schTasks /query /tn "MongoDB database continous backup" >nul 2>&1
+if %ERRORLEVEL% EQU 0 goto HELPERTASK
 schTasks /create /tn "MongoDB database continous backup" /tr "wscript.exe %SystemDrive%\scripts\hide_backup.vbs" /sc HOURLY /mo 2 /rl HIGHEST
-schTasks /create /tn "Meteor helper application" /tr "wscript.exe %SystemDrive%\scripts\hide_helper.vbs" /sc ONLOGON /rl HIGHEST
+
+:HELPERTASK
+schTasks /query /tn "Meteor helper application" >nul 2>&1
+if %ERRORLEVEL% EQU 0 goto TASKSEND
+schTasks /create /tn "Meteor helper application" /tr "wscript.exe %SystemDrive%\scripts\hide_helper.vbs" /sc ONSTART /rl HIGHEST
+
+:TASKSEND
+echo.
+echo Tasks creation ended
+echo.
 
 pause

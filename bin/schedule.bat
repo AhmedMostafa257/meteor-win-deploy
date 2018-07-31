2f7beb08d5bc3d88d4ad0f1ded83607d8124aa8b
Ahmed Mostafa Gomaa <ahmedmostafa257@gmail.com>
2018-07-31 17:11:47 +0200

    Allow tasks to run wheather a user is logged on or not

--- bin/schedule.bat
+++ bin/schedule.bat
@@ -2,12 +2,12 @@
 
 schTasks /query /tn "MongoDB database continous backup" >nul 2>&1
 if %ERRORLEVEL% EQU 0 goto HELPERTASK
-schTasks /create /tn "MongoDB database continous backup" /tr "wscript.exe %SystemDrive%\scripts\hide_backup.vbs" /sc HOURLY /mo 2 /rl HIGHEST
+schTasks /create /tn "MongoDB database continous backup" /tr "wscript.exe %SystemDrive%\scripts\hide_backup.vbs" /sc HOURLY /mo 2 /ru "NT AUTHORITY\SYSTEM" /np /rl HIGHEST
 
 :HELPERTASK
 schTasks /query /tn "Meteor helper application" >nul 2>&1
 if %ERRORLEVEL% EQU 0 goto TASKSEND
-schTasks /create /tn "Meteor helper application" /tr "wscript.exe %SystemDrive%\scripts\hide_helper.vbs" /sc ONSTART /rl HIGHEST
+schTasks /create /tn "Meteor helper application" /tr "wscript.exe %SystemDrive%\scripts\hide_helper.vbs" /sc ONSTART /ru "NT AUTHORITY\SYSTEM" /np /rl HIGHEST
 
 :TASKSEND
 echo.

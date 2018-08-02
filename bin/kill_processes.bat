727617606f0c6e3e17281adf7af38fd916da1f27
Ahmed Mostafa Gomaa <ahmedmostafa257@gmail.com>
2018-08-02 16:33:36 +0200

    Tiny fix in kill processes batch file

--- bin/kill_processes.bat
+++ bin/kill_processes.bat
@@ -8,15 +8,25 @@ echo ..................................................
 echo.
 
 for %%a in ("C:\scripts\pids\*") do (
+  echo PID from file is %%~nxa
   for /f "usebackq tokens=1" %%h in (`tasklist ^| findstr %%~nxa`) do (
     echo Process name is %%h
     if %%h==cmd.exe taskkill /T /F /PID %%h
     if %%h==node.exe taskkill /T /F /PID %%h
   )
-  echo PID from file is %%~nxa
   del /f %%a
 )
 
-if %UNATTENDED% EQU 0 pause
+for /f "usebackq tokens=2 delims= " %%a in (`tasklist ^| findstr node.exe`) do (
+    echo.
+    echo Terminating node processes ...
+    echo.
+    echo Process PID is %%a
+    taskkill /T /F /PID %%a
+  )
+
+echo.
+echo Done
+echo.
 
 exit

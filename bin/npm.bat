2b1b7532a9659618cb03651e21afec7c2d7baea8
Ahmed Mostafa Gomaa <ahmedmostafa257@gmail.com>
2018-08-02 17:20:25 +0200

    Tiny fix related to safe code

--- bin/npm.bat
+++ bin/npm.bat
@@ -7,34 +7,33 @@ echo Installing node modules
 echo .......................
 echo.
 
-if [%UNATTENDED%]==[] (
+if [%UNATTENDED%]==[] goto CONFIGPYTHON
   if %UNATTENDED% EQU 0 (
     echo Updating NPM ...
     echo.
     cmd /c npm i -g npm
     echo.
   )
-)
 
+:CONFIGPYTHON
 echo.
 npm config set python python2.7 -g
 npm config set msvs_version 2015 --global
 echo.
 
-if [%HELPER%]==[] (
+if [%HELPER%]==[] goto INSTALLHELPERMODULES
   if %HELPER% EQU 0 (
     goto INSTALLAPPNPM
   )
-)
 
+:INSTALLHELPERMODULES
 echo Installing helper node modules ...
 echo.
 cd "%SystemDrive%\helper"
 cmd /c npm i
-if [%UNATTENDED%]=[] (
+if [%UNATTENDED%]=[] goto INSTALLAPPNPM
   if %UNATTENDED% EQU 0 cmd /c npm audit fix
   if %UNATTENDED% EQU 0 cmd /c npm audit
-)
 echo.
 
 :INSTALLAPPNPM
@@ -46,17 +45,18 @@ if [%INSTALLDIR%]==[] (
   cd "%SystemDrive%\var\www\meteor\bundle\programs\server"
 )
 cmd /c npm i
-if [%UNATTENDED%]==[] (
+if [%UNATTENDED%]==[] goto FINISH
   if %UNATTENDED% EQU 0 cmd /c npm audit fix
   if %UNATTENDED% EQU 0 cmd /c npm audit
-)
 echo.
 
-echo done
-
-if [%UNATTENDED%]==[] (
   if %UNATTENDED% EQU 0 (
     pause
     exit
   )
-)
+
+:FINISH
+
+echo.
+echo done
+echo.

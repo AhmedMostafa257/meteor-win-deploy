e52821aa632dcca1bc71734b66c72434e529bb0e
Ahmed Mostafa Gomaa <ahmedmostafa@gmail.com>
2018-08-12 08:48:54 +0200

    Fixes in npm install batch file related to variable declaration

--- bin/npm.bat
+++ bin/npm.bat
@@ -6,53 +6,63 @@ echo Installing node modules
 echo .......................
 echo.
 
-if not defined %UNATTENDED% if %UNATTENDED% EQU 0 (
-  echo.
-  echo Updating NPM ...
-  echo.
-  cmd /c npm i -g npm
-  echo.
-)
+if [%UNATTENDED%]==[] goto UPDATENPM
 
+if %UNATTENDED% EQU 1 goto NODEGYPFIX
+
+:UPDATENPM
+echo.
+echo Updating NPM ...
+echo.
+cmd /c npm i -g npm
+echo.
+
+:NODEGYPFIX
 echo.
 echo Fixing Python issue for node-gyp ...
 cmd /c npm config set python python2.7 -g
 cmd /c npm config set msvs_version 2015 --global
 echo.
 
-if not defined %HELPER% if %HELPER% EQU 1 (
+if [%HELPER%]==[] goto INSTALLAPPMODULES
+
+if %HELPER% EQU 1 (
   echo.
   echo Installing helper node modules ...
   echo.
   cd "%SystemDrive%\helper"
   cmd /c npm i
-)
+  if [%UNATTENDED%]==[] (
+    goto INSTALLAPPMODULES
+  ) else goto CHECKUNATTENDEDAGAIN
+) else goto INSTALLAPPMODULES
 
-if not defined %UNATTENDED% if %UNATTENDED% EQU 0 (
+:CHECKUNATTENDEDAGAIN
+if %UNATTENDED% EQU 0 (
   cmd /c npm audit fix
   cmd /c npm audit
   echo.
 )
 
+:INSTALLAPPMODULES
 echo.
 echo Installing Meteor.JS application node modules ...
 echo.
 if [%INSTALLDIR%]==[] (
   cd %INSTALLDIR%\bundle\programs\server\
-) else (
-  cd %SystemDrive%\var\www\meteor\bundle\programs\server
-)
+) else cd %SystemDrive%\var\www\meteor\bundle\programs\server
 
 cmd /c npm i
 
-if not defined %UNATTENDED% if %UNATTENDED% EQU 0 (
+if [%UNATTENDED%]==[] goto FINISH
+
+if %UNATTENDED% EQU 0 (
   cmd /c npm audit fix
   cmd /c npm audit
   echo.
-  pause
-  exit
 )
 
+:FINISH
 echo.
 echo done
 echo.
